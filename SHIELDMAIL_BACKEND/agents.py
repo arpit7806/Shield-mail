bff65322a2ce04fdf4f3ab5b5cf441421aad815d
arpit7806 <arpitofficial7806@gmail.com>
2026-06-26 12:31:48 +0530

    Update agents.py

--- SHIELDMAIL_BACKEND/agents.py
+++ SHIELDMAIL_BACKEND/agents.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatGroq(
-    model="llama-3.1-8b-instant",",
+    model="llama-3.1-8b-instant",
     api_key=os.getenv("GROQ_API_KEY"),
     temperature=0.2
 )
